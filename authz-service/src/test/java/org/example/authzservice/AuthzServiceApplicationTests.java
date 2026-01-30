e08b27f22d037a6f8643b8d6a5d96142827cbf61
Manpreet <contact@manpreetsingh.dev>
2026-01-30 14:27:13 -0500

    add initial project files for like, post, authz, config, gateway, and profile services with Maven wrapper

---
+++ authz-service/src/test/java/org/example/authzservice/AuthzServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.authzservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthzServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
