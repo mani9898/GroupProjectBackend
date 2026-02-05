42e02a6f70ffee0b3a3fe5a06b8325ff7560ea70
Manpreet <contact@manpreetsingh.dev>
2026-02-05 11:56:41 -0500

    rename BackEndToBackController to AuthzController and update follower check endpoint to use POST method

--- authz-service/src/main/java/org/example/authzservice/controller/AuthController.java
+++ authz-service/src/main/java/org/example/authzservice/controller/AuthController.java
@@ -11,15 +11,13 @@ import org.example.authzservice.service.SocialMediaUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
+@CrossOrigin("http://localhost:3000")
 public class AuthController {
 
 
